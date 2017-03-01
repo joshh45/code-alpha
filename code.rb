f6d66ab4d9a76de35040e39e1bcf40f611d5eeba
Josh Ho <hojo1025@gmail.com>
2017-03-01 11:44:46 -0500

    iteration through hash, recursion logic, push slice portion into head, keep track of grammer

--- code.rb
+++ code.rb
@@ -2,3 +2,22 @@ grammar = {
   noun: ["abcd", "c", "def", "h", "ij", "cde"],
   verb: ["bc", "fg", "g", "hij", "bcd"],
   article:  ["a", "ac", "e"]}
+
+
+def return_setences(string, grammar, head = [], structure = [])
+  array = [];
+  if string.empty?
+    puts "placeholder for logic"
+  else
+    grammar.each do |type, words|
+      words.each do |word|
+        if string.start_with?(word)
+          return_setences(string.slice(word.size..-1), grammar, head + [word], structure + [type])
+        end
+      end
+    end
+  end
+end
+
+
+return_setences("abcdefg", grammar)
